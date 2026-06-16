87fbe9bbac5284a8ed5f206efd4eeacfc38b3567
alexander vela yayo <alexandervelamaldo@gmail.com>
2026-06-16 16:37:03 -0400

    Update app_router.dart

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
+import '../../features/asistente/screens/asistente_screen.dart';
 import '../../features/auth/screens/login_screen.dart';
 import '../../features/auth/screens/register_screen.dart';
 import '../../features/auth/screens/splash_screen.dart';
 import '../../features/compras/screens/home_screen.dart';
 import '../../features/compras/screens/purchase_history_screen.dart';
 import '../../features/compras/screens/purchase_screen.dart';
+import '../../features/reportes/screens/reportes_screen.dart';
+import '../../features/dashboard/screens/admin_dashboard_screen.dart';
+import '../../features/monitoreo/screens/monitoreo_screen.dart';
+import '../../features/inventario/screens/inventario_screen.dart';
 
 class AppRoutes {
   static const splash = '/';
@@ -14,6 +19,11 @@ class AppRoutes {
   static const home = '/home';
   static const purchase = '/purchase';
   static const history = '/history';
+  static const reportes = '/reportes';
+  static const monitoreo = '/monitoreo';
+  static const adminDashboard = '/dashboard';
+  static const inventario = '/inventario';
+  static const asistente = '/asistente';
 }
 
 class AppRouter {
@@ -31,6 +41,16 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => const PurchaseScreen());
       case AppRoutes.history:
         return MaterialPageRoute(builder: (_) => const PurchaseHistoryScreen());
+      case AppRoutes.reportes:
+        return MaterialPageRoute(builder: (_) => const ReportesScreen());
+      case AppRoutes.monitoreo:
+        return MaterialPageRoute(builder: (_) => const MonitoreoScreen());
+      case AppRoutes.adminDashboard:
+       return MaterialPageRoute(builder: (_) => const AdminDashboardScreen());
+       case AppRoutes.inventario:
+        return MaterialPageRoute(builder: (_) => const InventarioScreen());
+      case AppRoutes.asistente:
+        return MaterialPageRoute(builder: (_) => const AsistenteScreen());
       default:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
     }
